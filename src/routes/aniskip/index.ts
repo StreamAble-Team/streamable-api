3b699b028508e637030559bd77deca8abfb79c9d
Tommy Danks <tommydanks2000@hotmail.com>
2023-02-14 15:54:00 +0000

    aniskip

---
+++ src/routes/aniskip/index.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import {
+  FastifyRequest,
+  FastifyReply,
+  FastifyInstance,
+  RegisterOptions,
+} from "fastify";
+
+import { cache } from "../../utils";
+
+const routes = async (fastify: FastifyInstance, opts: RegisterOptions) => {
+  const api = (malid: number, episode: number) =>
+    `https://api.aniskip.com/v2/skip-times/${malid}/${episode}?types[]=ed&types[]=mixed-ed&types[]=mixed-op&types[]=op&types[]=recap&episodeLength=`;
+
+  fastify.get(
+    "/:malid/:episode",
+    async (request: FastifyRequest, reply: FastifyReply) => {
+      const { malid, episode } = request.params as {
+        malid: number;
+        episode: number;
+      };
+
+      const url = await api(malid, episode);
+
+      const { data } = await axios.get(url);
+
+      if (data?.found === false)
+        return reply.code(404).send({ error: "Not Found" });
+
+      if (data?.results?.length === 0)
+        return reply.code(204).send({
+          error: "No results found",
+        });
+
+      return data?.results;
+    }
+  );
+};
+
+export default routes;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,8 @@ import list from "./list";
 import anilist from "./anilist";
 import anilistManga from "./anilist/manga";
 
+import aniskip from "./aniskip";
+
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get("/", async (request, reply) => {
     return { message: "Hello World" };
@@ -12,6 +14,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.register(list, { prefix: "/list" });
   fastify.register(anilist, { prefix: "/anilist" });
   fastify.register(anilistManga, { prefix: "/anilist-manga" });
+  fastify.register(aniskip, { prefix: "/aniskip" });
 };
 
 export default routes;
