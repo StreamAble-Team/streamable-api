9e19972e1ab49451c4b6b114f94f999871cb31b7
Tommy <tommydanks2000@outlook.com>
2023-10-06 23:30:13 +0100

    pleaseeeeeeeee

--- src/routes/info/anilist/index.ts
+++ src/routes/info/anilist/index.ts
@@ -2,7 +2,11 @@ import { FastifyInstance, FastifyReply, FastifyRequest, RegisterOptions } from "
 import { cache } from "../../../utils";
 import { ANIME, Genres, META, PROVIDERS_LIST, StreamingServers } from "@consumet/extensions";
 import NineAnime from "@consumet/extensions/dist/providers/anime/9anime";
-import Anilist from "@consumet/extensions/dist/providers/meta/anilist";
+import CAnilist from "@consumet/extensions/dist/providers/meta/anilist";
+
+import AAnilist from "apollotv-providers/dist/extensions/info/anilist";
+
+import { INFO } from "apollotv-providers";
 
 const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
@@ -52,7 +56,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
 
     if (!id) return reply.code(400).send({ error: "ID parameter is required" });
 
-    let anilist = generateAnilistMeta(provider);
+    let anilist = generateAnilistMetav2();
 
     if (isDub === "true" || isDub === "1") isDub = true;
     else isDub = false;
@@ -62,12 +66,12 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
 
     try {
       let data = await cache.fetch(
-        `anilist:info;${id};${isDub};${fetchFiller};${anilist.provider.name.toLowerCase()}`,
-        async () => await anilist.fetchAnimeInfo(id, isDub as boolean, fetchFiller as boolean),
+        `anilist:info;${id};${isDub};${fetchFiller};${anilist.provider.metaData.name.toLowerCase()}`,
+        async () => await anilist.getMediaInfo(id, isDub as boolean),
         today === 0 || today === 6 ? 60 * 120 : (60 * 60) / 2
       );
 
-      data = data ? data : await anilist.fetchAnimeInfo(id, isDub, fetchFiller);
+      data = data ? data : await anilist.getMediaInfo(id, isDub);
 
       return reply.code(200).send(data);
     } catch (err) {
@@ -369,7 +373,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
   });
 };
 
-const generateAnilistMeta = (provider: string | undefined = undefined): Anilist => {
+const generateAnilistMeta = (provider: string | undefined = undefined): CAnilist => {
   if (typeof provider !== "undefined") {
     let possibleProvider = PROVIDERS_LIST.ANIME.find(
       (p: any) => p.name.toLowerCase() === provider.toLocaleLowerCase()
@@ -389,10 +393,15 @@ const generateAnilistMeta = (provider: string | undefined = undefined): Anilist 
       url: process.env.PROXY as string | string[],
     });
   } else {
-    return new Anilist(undefined, {
+    return new CAnilist(undefined, {
       url: process.env.PROXY as string | string[],
     });
   }
+};
+
+const generateAnilistMetav2 = (): AAnilist => {
+  console.log(process.env.ANIMAPPED_API_KEY!);
+  return new INFO.Anilist(undefined, process.env.ANIMAPPED_API_KEY!);
 };
 
 export default routes;
